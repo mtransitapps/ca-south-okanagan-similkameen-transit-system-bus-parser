f4abd8fb283b5f004b849824ed767ac98e866f73
Mathieu Méa <mathieu.mea@gmail.com>
2022-03-09 07:42:34 -0500

    Initial release.

---
+++ src/main/java/org/mtransit/parser/ca_south_okanagan_similkameen_transit_system_bus/SouthOkanaganSimilkameenTransitSystemBusAgencyTools.java
@@ -0,0 +1,122 @@
+package org.mtransit.parser.ca_south_okanagan_similkameen_transit_system_bus;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.mtransit.commons.CleanUtils;
+import org.mtransit.parser.DefaultAgencyTools;
+import org.mtransit.parser.MTLog;
+import org.mtransit.parser.gtfs.data.GRoute;
+import org.mtransit.parser.mt.data.MAgency;
+
+import java.util.regex.Pattern;
+
+import static org.mtransit.commons.StringUtils.EMPTY;
+
+// https://www.bctransit.com/open-data
+// https://www.bctransit.com/data/gtfs/south-okanagan-similkameen.zip
+public class SouthOkanaganSimilkameenTransitSystemBusAgencyTools extends DefaultAgencyTools {
+
+	public static void main(@NotNull String[] args) {
+		new SouthOkanaganSimilkameenTransitSystemBusAgencyTools().start(args);
+	}
+
+	@Override
+	public boolean defaultExcludeEnabled() {
+		return true;
+	}
+
+	@NotNull
+	@Override
+	public String getAgencyName() {
+		return "South Okanagan-Similkameen TS";
+	}
+
+	@NotNull
+	@Override
+	public Integer getAgencyRouteType() {
+		return MAgency.ROUTE_TYPE_BUS;
+	}
+
+	@Override
+	public boolean defaultRouteIdEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean useRouteShortNameForRouteId() {
+		return true;
+	}
+
+	@Override
+	public boolean defaultAgencyColorEnabled() {
+		return true;
+	}
+
+	private static final String AGENCY_COLOR_GREEN = "34B233";// GREEN (from PDF Corporate Graphic Standards)
+	private static final String AGENCY_COLOR_BLUE = "002C77"; // BLUE (from PDF Corporate Graphic Standards)
+
+	private static final String AGENCY_COLOR = AGENCY_COLOR_GREEN;
+
+	@NotNull
+	@Override
+	public String getAgencyColor() {
+		return AGENCY_COLOR;
+	}
+
+	@Nullable
+	@Override
+	public String provideMissingRouteColor(@NotNull GRoute gRoute) {
+		switch (gRoute.getRouteShortName()) {
+		// @formatter:off
+		case "1": return "80CC28";
+		case "2": return "17468b";
+		case "3": return "F68712";
+		case "4": return "ED0790";
+		case "5": return "A32B9B";
+		case "10": return "FFC10E";
+		case "11": return "E370AC";
+		case "15": return "ACA86E";
+		case "16": return "00ADEF";
+		case "20": return "F38062";
+		case "30": return "5DA8DC";
+		case "40": return "81955B";
+		case "50": return "874d9D";
+		case "70": return "49176D";
+		// @formatter:on
+		}
+		throw new MTLog.Fatal("Unexpected route color for %s!", gRoute.toStringPlus());
+	}
+
+	@Override
+	public boolean directionFinderEnabled() {
+		return true;
+	}
+
+	private static final Pattern STARTS_W_RLN_DASH_ = Pattern.compile("^.+- ", Pattern.CASE_INSENSITIVE);
+
+	private static final Pattern ENDS_WITH_DASH_ = Pattern.compile("-$", Pattern.CASE_INSENSITIVE);
+
+	@NotNull
+	@Override
+	public String cleanTripHeadsign(@NotNull String tripHeadsign) {
+		tripHeadsign = CleanUtils.keepToAndRemoveVia(tripHeadsign);
+		tripHeadsign = STARTS_W_RLN_DASH_.matcher(tripHeadsign).replaceAll(EMPTY);
+		tripHeadsign = ENDS_WITH_DASH_.matcher(tripHeadsign).replaceAll(EMPTY);
+		tripHeadsign = CleanUtils.CLEAN_AT.matcher(tripHeadsign).replaceAll(CleanUtils.CLEAN_AT_REPLACEMENT);
+		tripHeadsign = CleanUtils.CLEAN_AND.matcher(tripHeadsign).replaceAll(CleanUtils.CLEAN_AND_REPLACEMENT);
+		tripHeadsign = CleanUtils.cleanStreetTypes(tripHeadsign);
+		tripHeadsign = CleanUtils.cleanNumbers(tripHeadsign);
+		return CleanUtils.cleanLabel(tripHeadsign);
+	}
+
+	@NotNull
+	@Override
+	public String cleanStopName(@NotNull String gStopName) {
+		gStopName = CleanUtils.CLEAN_AT.matcher(gStopName).replaceAll(CleanUtils.CLEAN_AT_REPLACEMENT);
+		gStopName = CleanUtils.CLEAN_AND.matcher(gStopName).replaceAll(CleanUtils.CLEAN_AND_REPLACEMENT);
+		gStopName = CleanUtils.cleanBounds(gStopName);
+		gStopName = CleanUtils.cleanNumbers(gStopName);
+		gStopName = CleanUtils.cleanStreetTypes(gStopName);
+		return CleanUtils.cleanLabel(gStopName);
+	}
+}
